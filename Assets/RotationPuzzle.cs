6dc00dd870c330e162ce9a66867c2cbdc0cf8261
ChoiKangGeon <gongun99@gmail.com>
2024-08-07 15:58:14 +0900

    [Update] 퍼즐리소스 변경

--- Assets/RotationPuzzle.cs
+++ Assets/RotationPuzzle.cs
@@ -6,8 +6,38 @@ using UnityEngine.EventSystems;
 public class RotationPuzzle : MonoBehaviour , IPointerClickHandler
 {
     private float rotationAngle = 90f;
+
+    private float rotationDuration = 0.5f;
+
+    private bool isRotating = false;
+
+    private Quaternion targetRotation;
+
+    private void Start()
+    {
+        targetRotation = transform.rotation;
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        gameObject.transform.Rotate(0, 0, -rotationAngle);
+        targetRotation *= Quaternion.Euler(0, 0, -rotationAngle);
+        StartCoroutine(RotateOverTime(targetRotation, rotationDuration));
+    }
+
+    private IEnumerator RotateOverTime(Quaternion endRotation, float duration)
+    {
+        isRotating = true;
+
+        Quaternion startRotation = transform.rotation;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+        isRotating = false;
     }
 }
