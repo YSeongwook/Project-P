5f26c85fce7ab9c31c09a4473ce0a23673233fc1
SeongWook <9892yyyssww@gmail.com>
2024-09-12 15:37:03 +0900

    [Add] UI-Rounded-Corners 에셋 추가

---
+++ Assets/02.Script/MiniGame/Obejct/Feed.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feed : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    
+    // Todo: 드래그 앤 드랍으로 소에게 놓을 시 비활성화
+
+}
