972731bb3b2dbe68cb1a3d657887f0df9ad2a304
조현승 <danitech171791@gmail.com>
2024-09-30 16:56:36 +0900

    [feat] Meta - 네트워크 플레이어 구현
    - 애니메이션 상태 반영
    - 화면 제어 처리
    - 회전, 이동 처리

---
+++ Assets/8.Metaverse/Scripts/NetPlayer.cs
@@ -0,0 +1,143 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NetPlayer : NetworkBehaviour
+{
+    [Header("Components")]
+    public NavMeshAgent NavAgent_Player;
+    public Animator Animator_Player;
+
+    [Header("Movement")]
+    public float _rotationSpeed = 100.0f;
+
+    [Header("Interaction")]
+    public KeyCode _atkKey = KeyCode.Space;
+
+    [SerializeField] Rigidbody RigidBody_Player;
+    [SerializeField] Camera Camera_Player;
+
+    private float _moveSpeed = 5.0f;
+    private float _mouseSensitivity = 100.0f;
+    private float _cameraRotationX = 0.0f;
+
+    private void Start()
+    {
+        if (isLocalPlayer == false)
+        {
+            Camera_Player.gameObject.SetActive(false);
+            return;
+        }
+
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+
+    private void Update()
+    {
+        if(CheckIsFocusedOnUpdate() == false)
+        {
+            return;
+        }
+
+        //CheckIsLocalPlayerOnUpdate();
+        MoveOnUpdate();
+    }
+
+    private bool CheckIsFocusedOnUpdate()
+    {
+        return Application.isFocused;
+    }
+
+    private void MoveOnUpdate()
+    {
+        if (isLocalPlayer == false)
+            return;
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        bool isRunning = movement.magnitude > 0;
+        RequestCommandAnimChange("Run", isRunning);
+
+        // Rotate Camera
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+
+        _cameraRotationX -= mouseY;
+        _cameraRotationX = Mathf.Clamp(_cameraRotationX, -90f, 90f);
+
+        Camera_Player.transform.localRotation = Quaternion.Euler(_cameraRotationX, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Root Anim - True
+        // this.transform.Translate(Vector3.right * (moveHorizontal * _moveSpeed * Time.deltaTime));
+        // this.transform.Translate(Vector3.forward * (moveVertical * _moveSpeed * Time.deltaTime));
+    }
+
+    private void CheckIsLocalPlayerOnUpdate()
+    {
+        if (this.isLocalPlayer == false)
+            return;
+
+        // 회전
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(0, horizontal * _rotationSpeed * Time.deltaTime, 0);
+
+        // 이동
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        NavAgent_Player.velocity = forward * Mathf.Max(vertical, 0) * NavAgent_Player.speed;
+        Animator_Player.SetBool("Moving", NavAgent_Player.velocity != Vector3.zero);
+
+        // 공격
+        if (Input.GetKeyDown(_atkKey))
+        {
+            RequestCommandInteractionOnServer();
+        }
+
+        RotateLocalPlayer();
+    }
+
+    [Command]
+    void RequestCommandAnimChange(string animKey, bool isStart)
+    {
+        RpcOnAnimChange(animKey, isStart);
+    }
+
+    [ClientRpc]
+    void RpcOnAnimChange(string animStateKey, bool isStart)
+    {
+        Animator_Player.SetBool(animStateKey, isStart);
+    }
+
+    [Command]
+    void RequestCommandInteractionOnServer()
+    {
+        //GameObject atkObjectForSpawn = Instantiate(Prefab_AtkObject, Tranfrom_AtkSpawnPos.transform.position, Tranfrom_AtkSpawnPos.transform.rotation);
+        //NetworkServer.Spawn(atkObjectForSpawn);
+
+        //RpcOnInteract("Atk");
+    }
+
+    [ClientRpc]
+    void RpcOnInteract(string animStateKey)
+    {
+        Animator_Player.SetTrigger(animStateKey);
+    }
+
+    void RotateLocalPlayer()
+    {
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(ray, out RaycastHit hit, 100))
+        //{
+        //    Debug.DrawLine(ray.origin, hit.point);
+        //    Vector3 lookRotate = new Vector3(hit.point.x, Transform_Player.position.y, hit.point.z);
+        //    Transform_Player.LookAt(lookRotate);
+        //}
+    }
+}
