3faac6f1fbeb800ea8e3f5c923cf7c89c427c754
조현승 <danitech171791@gmail.com>
2024-10-02 10:41:18 +0900

    [feat] Meta - 네트워크 인터렉션 UI

---
+++ Assets/8.Metaverse/Scripts/InteractUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InteractUI : MonoBehaviour
+{
+    [SerializeField] private string Str_GameStartSceneName;
+
+    public void OnClick_StartGame()
+    {
+        SceneManager.LoadScene(Str_GameStartSceneName);
+    }
+
+    public void OnClick_CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void OnClick_CreateInteractObj()
+    {
+
+    }
+
+    public void OnClick_InteractMotion()
+    {
+
+    }
+}
